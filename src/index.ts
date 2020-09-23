a7688bbafcd833b21652919036d04a9b3572c2a2
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-09-22 20:21:59 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,292 @@
+import { ILogger } from 'the-logger';
+import { Promise } from 'the-promise';
+import _ from 'the-lodash';
+import axios from 'axios';
+import { AxiosResponse } from 'axios';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+interface JobInfo 
+{
+    name: string;
+    seconds: number;
+    cb: (...args: any[]) => void;
+    preScheduleCb?: (...args: any[]) => void;
+}
+
+interface JobRuntime 
+{
+    isRunning: boolean
+}
+
+interface ErrorInfo
+{
+    error: string;
+    count: number;
+}
+
+enum ReportActions {
+    VersionCheck = 'version-check',
+    ReportError = 'report-error',
+    ReportMetrics = 'report-metrics',
+}
+
+export class WorldviousClient
+{
+    private logger : ILogger;
+    private name : string;
+    private version : string;
+    private enabled = true;
+    private id? : string;
+    private closed = false;
+    private isFirstErrorReported = false;
+
+    private timers : Record<string, NodeJS.Timeout> = {};
+    private jobs : Record<string, JobInfo> = {};
+    private jobRuntime : Record<string, JobRuntime> = {};
+
+    private counters = {};
+    private metrics = {};
+    private errors : Record<string, ErrorInfo> = {};
+
+    constructor(logger : ILogger, name: string, version: string)
+    {
+        this.logger = logger.sublogger("Worldvious");
+        this.name = name;
+        this.version = version;
+
+        this.id = process.env.WORLDVIOUS_ID;
+        if (!this.id) {
+            this.logger.warn("NO WORLDVIOUS_ID Not Set. Disabling.");
+            this.enabled = false;
+        }
+
+        if (!process.env.WORLDVIOUS_URL) {
+            this.logger.warn("NO WORLDVIOUS_URL Set. Disabling.");
+            this.enabled = false;
+        }
+
+    }
+
+    init() : Promise<any>
+    {
+        return Promise.resolve()
+            .then(() => this._schedule())
+            .then(() => this._runJobNow(ReportActions.VersionCheck));
+    }
+
+    close()
+    {
+        this.logger.info("Closing...")
+        this.closed = true;
+        for(let timer of _.values(this.timers))
+        {
+            clearTimeout(timer);
+        }
+        this.timers = {};
+    }
+
+    acceptError(error: any)
+    {
+        this.logger.info("*** ACCEPT ERROR");
+
+        const errorStr = this._getErrorString(error);
+        if (this.errors[errorStr]) {
+            this.errors[errorStr].count = this.errors[errorStr].count + 1;
+        } else {
+            this.errors[errorStr] = {
+                error: errorStr,
+                count: 1
+            };
+        }
+        // this.logger.info("*** ACCEPT ERROR end:", this.errors);
+
+        if (!this.isFirstErrorReported) {
+            this.isFirstErrorReported = true;
+            this._runJobNow(ReportActions.ReportError);
+        }
+    }
+
+    private _runJobNow(name : string) : Promise<any> | null
+    {
+        return this._runJob(name, true);
+    }
+
+    private _schedule()
+    {
+        this._register(ReportActions.VersionCheck, 60 * 60, 'WORLDVIOUS_VERSION_CHECK_TIMEOUT', () => {
+            return this._checkVersion();
+        });
+
+        this._register(ReportActions.ReportError, 1 * 60, 'WORLDVIOUS_ERROR_REPORT_TIMEOUT', () => {
+            return this._reportError();
+        }, () => {
+            this.isFirstErrorReported = false;
+        });
+    }
+
+
+    private _checkVersion()
+    {
+        const data = this._makeNewData();
+        data.version = this.version;
+        return this._request('report/version', data)
+            .then(result => {
+                this.logger.info("VersionCheckResult: ", result);
+            });
+    }
+
+
+    private _reportError()
+    {
+        this.logger.info("_reportError ", this.errors);
+        if (_.keys(this.errors).length == 0) {
+            return;
+        }
+
+        const payloads : any[] = [];
+
+        for(let errorInfo of _.values(this.errors))
+        {
+            const data = this._makeNewData();
+            data.version = this.version;
+            data.error = errorInfo.error;
+            data.count = errorInfo.count;
+            payloads.push(data);
+        }
+        this.errors = {};
+
+        return Promise.serial(payloads, data => {
+                return this._request('report/error', data);
+            })
+            .then(result => {
+                this.logger.info("VersionCheckResult: ", result);
+            });
+    }
+
+    private _getErrorString(error : any)
+    {
+        if (_.isNullOrUndefined(error)) {
+            return "UNDEFINED ERROR";
+        }
+        if (error.stack) {
+            return error.stack;
+        }
+        return error.toString();
+    }
+
+
+    private _runJob(name : string, executeNow: boolean) : Promise<any> | null
+    {
+        const job = this.jobs[name];
+        if (!job) {
+            this.logger.error("Unknown job: %s.", name);
+            return null;
+        }
+
+        const handler = () => {
+            if (this.jobRuntime[name].isRunning) {
+                this._runJob(name, false);
+                return null;
+            }
+            this.jobRuntime[name].isRunning = true;
+            this.logger.info("Running %s...", name);
+            const res = job.cb();
+            return Promise.resolve(res)
+                .then(() => {
+                    this.logger.info("Completed %s", name);
+                })
+                .catch(reason => {
+                    this.logger.error("%s failed. reason: ",name, reason);
+                })
+                .then(() => {
+                    this.jobRuntime[name].isRunning = false;
+                    this._runJob(name, false);
+                })
+        };
+
+        if (executeNow)
+        {
+            this._stopScheduledJob(name);
+            return handler();
+        }
+        else
+        {
+            if (!this.timers[name]) {
+                if (job.preScheduleCb) {
+                    job.preScheduleCb!();
+                }
+                const timer = setTimeout(() => {
+                    delete this.timers[name];
+                    handler();
+                }, job.seconds * 1000);
+                this.timers[name] = timer;
+            }
+        }
+
+        return null;
+    }
+
+    private _stopScheduledJob(name: string)
+    {
+        const timer = this.timers[name];
+        if (timer)
+        {
+            clearTimeout(timer);
+            delete this.timers[name];
+        }
+    }
+
+    private _register(name: string, seconds: number, overrideEnvName: string, cb: (...args: any[]) => void, preScheduleCb?: (...args: any[]) => void)
+    {
+        if (overrideEnvName) {
+            const overrideValue = process.env[overrideEnvName];
+            if (overrideValue)
+            {
+                const parsed = Number.parseInt(overrideValue);
+                if (!Number.isNaN(parsed))
+                {
+                    seconds = parsed;
+                }
+            }
+        }
+        this.logger.info("Registered %s. timeout: %ssec.", name, seconds);
+        this.jobs[name] = {
+            name: name,
+            seconds: seconds,
+            cb: cb,
+            preScheduleCb: preScheduleCb
+        }
+        this.jobRuntime[name] = {
+            isRunning: false
+        }
+    }
+
+    private _request(url: string, data: Record<string, any>) : Promise<AxiosResponse>
+    {
+        const fullUrl = `${process.env.WORLDVIOUS_URL}/${url}`;
+
+        this.logger.info("Requesting %s...", fullUrl, data);
+        return Promise.construct<AxiosResponse>((resolve, reject) => {
+            axios.post(fullUrl, data)
+                .then(result => {
+                    this.logger.info("Done %s.", fullUrl, result.data);
+                    resolve(result.data)
+                })
+                .catch(reason => {
+                    this.logger.error("Failed %s.", fullUrl, reason);
+                    reject(reason);
+                });
+        })
+    }
+
+    private _makeNewData() : Record<string, any>
+    {
+        const data : Record<string, any> = {}
+        data.id = this.id;
+        data.process = this.name;
+        return data;
+    }
+
+}
---
+++ test/client.ts
@@ -0,0 +1,257 @@
+import 'mocha';
+import should = require('should');
+
+import { Promise } from 'the-promise';
+import { setupLogger, LoggerOptions } from 'the-logger';
+const loggerOptions = new LoggerOptions().enableFile(false).pretty(true);
+const logger = setupLogger('test', loggerOptions);
+const serverLogger = setupLogger('SERVER', loggerOptions);
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const TEST_SERVER_PORT=4444
+process.env.WORLDVIOUS_URL=`http://localhost:${TEST_SERVER_PORT}/api/v1/oss`
+process.env.WORLDVIOUS_ID='123e4567-e89b-12d3-a456-426614174000'
+
+interface ServerMockData {
+    responses: Record<string, any>;
+    responseCounter: Record<string, number>;
+    server?: any
+};
+
+const SERVER_DATA : ServerMockData = {
+    responses: {},
+    responseCounter: {},
+    server: null
+};
+
+function registerRequest(name: string, body: any)
+{
+    SERVER_DATA.responses[name] = body;
+    if (SERVER_DATA.responseCounter[name]) {
+        SERVER_DATA.responseCounter[name] = SERVER_DATA.responseCounter[name] + 1;
+    } else {
+        SERVER_DATA.responseCounter[name] = 1;
+    }
+}
+
+import { WorldviousClient } from '../src';
+
+let client: WorldviousClient;
+
+describe('worldvious', () => {
+
+    beforeEach(() => {
+        var express = require("express");
+        var bodyParser = require('body-parser');
+        var app = express();
+
+        app.use(bodyParser.json())
+
+        app.post('/api/v1/oss/report/version', (req: any, res: any, next: any) => {
+            serverLogger.info("REPORT VERSION, body: ", req.body );
+            registerRequest('report-version', req.body);
+            res.json({
+                "newVersionPresent": true,
+                "version": "v1.2.3",
+                "url": "https://github.com/kubevious/helm#installing-the-chart-using-helm-v3x"
+            });
+        });
+
+        app.post('/api/v1/oss/report/error', (req: any, res: any, next: any) => {
+            serverLogger.info("REPORT ERROR, body: ", req.body );
+            registerRequest('report-error', req.body);
+            res.json({
+            });
+        });
+        
+        SERVER_DATA.responses = {};
+         
+        return Promise.construct((resolve, reject) => {
+            SERVER_DATA.server = app.listen(TEST_SERVER_PORT, () => {
+                console.log(`Server running on port ${TEST_SERVER_PORT}`);
+                resolve();
+            });
+        })
+        // .then(() => Promise.timeout(1000))
+    });
+
+    afterEach(() => {
+        if (client) {
+            client.close();
+        }
+        SERVER_DATA.server!.close();
+        SERVER_DATA.responses = {};
+        SERVER_DATA.responseCounter = {};
+        SERVER_DATA.server = null;
+    });
+
+    it('constructor', () => {
+        client = new WorldviousClient(logger, "kubevious", 'v1.2.3');
+        return Promise.resolve()
+    });
+
+    it('init', () => {
+        client = new WorldviousClient(logger, "kubevious", 'v1.2.3');
+        return Promise.resolve()
+            .then(() => client.init())
+    });
+
+    it('check_version_1', () => {
+        client = new WorldviousClient(logger, "kubevious", 'v1.2.3');
+        return Promise.resolve()
+            .then(() => client.init())
+            .then(() => {
+                should(SERVER_DATA.responses['report-version']).be.eql({
+                    id: "123e4567-e89b-12d3-a456-426614174000",
+                    process: "kubevious",
+                    version: "v1.2.3"
+                })
+            })
+    });
+
+    it('check_version_2', () => {
+        client = new WorldviousClient(logger, "parser", 'v7.8.9');
+        return Promise.resolve()
+            .then(() => client.init())
+            .then(() => {
+                should(SERVER_DATA.responses['report-version']).be.eql({
+                    id: "123e4567-e89b-12d3-a456-426614174000",
+                    process: "parser",
+                    version: "v7.8.9"
+                })
+            })
+    });
+
+    it('check_version_timeout', () => {
+        process.env.WORLDVIOUS_VERSION_CHECK_TIMEOUT = '1';
+        client = new WorldviousClient(logger, "parser", 'v7.8.9');
+        return Promise.resolve()
+            .then(() => client.init())
+            .then(() => Promise.timeout(5500))
+            .then(() => {
+                logger.info("Test end.");
+                should(SERVER_DATA.responseCounter['report-version']).be.equal(6);
+            })
+            .then(() => {
+                delete process.env.WORLDVIOUS_VERSION_CHECK_TIMEOUT;
+            })
+    })
+    .timeout(10 * 1000);
+
+    it('error_report_one', () => {
+        client = new WorldviousClient(logger, "parser", 'v7.8.9');
+        return Promise.resolve()
+            .then(() => client.init())
+            .then(() => {
+                try
+                {
+                    throw new Error("Something bad happened");
+                }
+                catch(reason)
+                {
+                    client.acceptError(reason);
+                }
+                
+            })
+            .then(() => Promise.timeout(100))
+            .then(() => {
+                should(SERVER_DATA.responseCounter['report-error']).be.equal(1);
+
+                const response = SERVER_DATA.responses['report-error'];
+
+                should(response.id).be.equal("123e4567-e89b-12d3-a456-426614174000");
+                should(response.process).be.equal("parser");
+                should(response.error).be.a.String();
+            })
+    });
+
+    it('error_report_multiple', () => {
+        process.env.WORLDVIOUS_ERROR_REPORT_TIMEOUT = '3';
+
+        client = new WorldviousClient(logger, "parser", 'v7.8.9');
+        return Promise.resolve()
+            .then(() => client.init())
+            .then(() => {
+                for(let i = 0; i < 10; i++)
+                {
+                    try
+                    {
+                        throw new Error(`Something bad happened - ${1}`);
+                    }
+                    catch(reason)
+                    {
+                        client.acceptError(reason);
+                    }
+                }
+            })
+            .then(() => Promise.timeout(100))
+            .then(() => {
+                should(SERVER_DATA.responseCounter['report-error']).be.equal(1);
+
+                const response = SERVER_DATA.responses['report-error'];
+
+                should(response.id).be.equal("123e4567-e89b-12d3-a456-426614174000");
+                should(response.process).be.equal("parser");
+                should(response.count).be.equal(1);
+                should(response.error).be.a.String();
+                should((<string>response.error).includes("Something bad happened"))
+            })
+            .then(() => Promise.timeout(3000))
+            .then(() => {
+                should(SERVER_DATA.responseCounter['report-error']).be.equal(2);
+
+                const response = SERVER_DATA.responses['report-error'];
+
+                should(response.id).be.equal("123e4567-e89b-12d3-a456-426614174000");
+                should(response.process).be.equal("parser");
+                should(response.count).be.equal(9);
+                should(response.error).be.a.String();
+                should((<string>response.error).includes("Something bad happened"))
+
+            })
+            .then(() => {
+                for(let i = 0; i < 5; i++)
+                {
+                    try
+                    {
+                        throw new Error(`One More Error - ${1}`);
+                    }
+                    catch(reason)
+                    {
+                        client.acceptError(reason);
+                    }
+                }
+            })
+            .then(() => Promise.timeout(100))
+            .then(() => {
+                should(SERVER_DATA.responseCounter['report-error']).be.equal(3);
+
+                const response = SERVER_DATA.responses['report-error'];
+
+                should((<string>response.error).includes("One More Error"))
+
+            })
+            .then(() => Promise.timeout(3000))
+            .then(() => {
+                should(SERVER_DATA.responseCounter['report-error']).be.equal(4);
+
+                const response = SERVER_DATA.responses['report-error'];
+
+                should(response.id).be.equal("123e4567-e89b-12d3-a456-426614174000");
+                should(response.process).be.equal("parser");
+                should(response.error).be.a.String();
+                should(response.count).be.equal(4);
+                should((<string>response.error).includes("One More Error"))
+            })
+    })
+    .timeout(20 * 1000);
+
+
+    // it('schedule', () => {
+    //     const client = new WorldviousClient(logger, "kubevious", 'v1.2.3');
+    //     client.close();
+    // });
+
+});
